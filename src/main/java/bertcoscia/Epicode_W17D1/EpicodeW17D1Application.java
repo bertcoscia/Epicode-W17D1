ec2ca884d6609ed4a3b464d2a11f5bb243cfdd2e
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-09-02 15:24:03 +0200

    Initial commit

---
+++ src/main/java/bertcoscia/Epicode_W17D1/EpicodeW17D1Application.java
@@ -0,0 +1,13 @@
+package bertcoscia.Epicode_W17D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeW17D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeW17D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/bertcoscia/Epicode_W17D1/EpicodeW17D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bertcoscia.Epicode_W17D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeW17D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
